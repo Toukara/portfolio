213af4fdcd38f4c45f41822ee6bf8d3fd40c24e0
Toukara <benjamincms62@gmail.com>
2024-09-06 04:51:03 +0200

    removed everything

--- src/components/LogoComponent.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="logo">
-    <router-link to="/">
-      <!-- <h1>benjamin <span>camus</span></h1> -->
-      <h1><span>B</span><span>C</span>.</h1>
-    </router-link>
-  </div>
-</template>
